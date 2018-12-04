7c06dae60f7777465cf52888be6b3da8931f6f5d
Ivan Dovhan <letsplayclash15@gmail.com>
2018-12-04 10:32:30 +0200

    updates

--- Lab2+/js/main.js
+++ Lab2+/js/main.js
@@ -2,5 +2,37 @@ $(document).ready(function ($) {
         $('#mob-trigger').click(function () {
             jQuery(".main-menu nav").slideToggle(500);
         });
+''
+        $('#message-submit').click(function () {
+            let messageText = $("#fan-message").val();
+            let message = $('<div class="some-message clearfix"></div>');
+
+            message.html('<div class="text"><p>' + messageText + '</p></div><div class="author">Formula1 Fan</div>');
+            message.appendTo('#main-messages');
+
+            let currentDate = new Date();
+            let datetime = currentDate.getDate() + "."
+                + (currentDate.getMonth() + 1) + "."
+                + currentDate.getFullYear() + ", "
+                + currentDate.getHours() + ":"
+                + currentDate.getMinutes();
+            let messageDate = $('<div class="date"></div>');
+
+            messageDate.html(datetime);
+            messageDate.appendTo('.some-message:last-child');
+
+            $('#fan-message').val('');
+            $('#message-submit').attr('disabled', true);
+        });
+
+        $('#message-submit').attr('disabled', true);
+        $('#fan-message').on('keyup', function () {
+            let textarea_value = $("#texta").val();
+            if (textarea_value !== '') {
+                $('#message-submit').attr('disabled', false);
+            } else {
+                $('#message-submit').attr('disabled', true);
+            }
+        });
     }
-);
+  );
